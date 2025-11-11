3adbdcbb6cf5d5c0a9aeca50b9eded3df00d15ec
stud106304 <christoph16hd@hotmail.com>
2025-11-11 13:11:37 +0100

    Add door prefabs and new environment models
    Added EB_DoorAnimated and EB_DoorProximity Variant prefabs with associated meta files and trigger script. Imported new environment models (CommsStation, FuzeBox, Garderobe, Labtable, Seat, SingleBed, SonarStation, Subdoor, Table) and updated the submarine scene and tag manager for integration.

---
+++ Unity/GameOff2025/Assets/_Project/Scripts/Door/DoorTrigger.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+/// <summary>
+/// This class is responsible for triggering door-related events. This is a temporary solution for opening and closing the doors.
+/// 
+/// Contains events for door open and close actions to which other scripts can subscribe and react to the event.
+/// </summary>
+public class DoorTrigger : MonoBehaviour
+{
+    // 1) Spieler geht in den Trigger
+    // 2) Tür öffnen
+    // 3) Spieler verlässte den Trigger
+    // 4) Tür schließen
+
+    [SerializeField] private GameObject _doorObject;
+    [SerializeField] private GameObject _doorframeObject;
+    // todo: add locked doors functionality
+    private Animator _doorAnimator;
+    // Events for subscribing to door open/close actions
+    // Usefull for sound effects or other interactions
+    public event Action OnDoorOpened;
+    public event Action OnDoorClosed;
+
+    void OnEnable()
+    {
+        _doorAnimator = _doorframeObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Creature"))
+        {
+            _doorAnimator.SetTrigger("openDoor");
+            _doorObject.GetComponent<Collider>().enabled = false;
+            OnDoorOpened?.Invoke();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Creature"))
+        {
+            _doorAnimator.SetTrigger("closeDoor");
+            _doorObject.GetComponent<Collider>().enabled = true;
+            OnDoorClosed?.Invoke();
+        }
+    }
+}
