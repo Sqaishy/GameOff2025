f1236f04dc7e9841406e528c96fa015416bdee70
stud106304 <christoph16hd@hotmail.com>
2025-11-13 01:38:46 +0100

    Add submarine lamp assets and update scene lighting
    Added new prefabs, models, and animations for submarine lamps, including LampIdle animation and SubLamp controller. Updated the 01_Submarine scene to include a SubLamp instance and enabled baked lighting with new LightingData. Also enabled collider generation for submarine hull models.

---
+++ Unity/GameOff2025/Assets/_Project/Scripts/Utility/LevelTransition.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+/**
+* This class handles the Teleportation of the Player between two points. Mostly used for transitioning in the submarine.
+*/
+public class LevelTransition : MonoBehaviour
+{
+    [SerializeField] private GameObject destination;
+    // For locking it until a condition is met.
+    // [SerializeField] private bool isLocked = false;
+    [SerializeField] private bool debugMode = false;
+
+    void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (debugMode)
+        {
+            Debug.Log("Player entered Teleporter... Waiting 3 seconds.");
+        }
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(delayedTeleport(other.gameObject));
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+        }
+    }
+
+    IEnumerator delayedTeleport(GameObject sourceObject)
+    {
+        yield return new WaitForSeconds(3);
+        teleport(sourceObject, destination);
+    }
+    
+    private void teleport(GameObject sourceObject, GameObject destination)
+    {
+        sourceObject.transform.position = destination.transform.position;
+        if (debugMode)
+        {
+            Debug.Log("Player teleported to: " + destination.transform.position);
+        }
+    }
+}
