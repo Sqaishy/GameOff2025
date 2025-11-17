9a20d20f0759fd444701219a87edd59333499c26
Sqaishy <JakeEbudd@icloud.com>
2025-11-17 23:04:18 +0000

    Fixed Interactor Context
    Removed duplicated interactor context

--- Unity/GameOff2025/Assets/_Project/Scripts/Interaction/InteractorContext.cs
+++ Unity/GameOff2025/Assets/_Project/Scripts/Interaction/InteractorContext.cs
@@ -4,9 +4,4 @@ namespace SubHorror.Interaction
 {
 	[CreateAssetMenu(menuName = "Sub Horror/Interaction/Interactor Context")]
 	public class InteractorContext : ScriptableObject { }
-
-	public abstract class ContextAction : ScriptableObject
-	{
-		public abstract void Execute(GameObject interactor);
-	}
 }
