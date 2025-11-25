7e758270cad883503120d0034b3bc9b2b2a894a5
Sqaishy <JakeEbudd@icloud.com>
2025-11-25 19:13:46 +0000

    Teleport Monster
    Now teleports the monster to the set destination when the player transitions floors

--- Unity/GameOff2025/Assets/_Project/Scripts/Utility/FloorTransition.cs
+++ Unity/GameOff2025/Assets/_Project/Scripts/Utility/FloorTransition.cs
@@ -1,10 +1,14 @@
 using SubHorror.Interaction;
+using SubHorror.Monster;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FloorTransition : MonoBehaviour, IInteractable
 {
 	[SerializeField] private InteractorContext interactorContext;
 	[SerializeField] private Transform destination;
+	[SerializeField] private Transform monsterDestination;
+	[SerializeField] private MonsterSpawner monsterSpawner;
 
 	public bool CanInteract()
 	{
@@ -27,5 +31,10 @@ public class FloorTransition : MonoBehaviour, IInteractable
 	private void TeleportToFloor(GameObject interactor)
 	{
 		interactor.transform.root.position = destination.transform.position;
+
+		if (!monsterSpawner.ActiveMonster)
+			return;
+
+		monsterSpawner.ActiveMonster.GetComponent<NavMeshAgent>().Warp(monsterDestination.position);
 	}
 }
